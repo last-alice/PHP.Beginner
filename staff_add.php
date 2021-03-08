0e137abdaa2bcf6bd5a74e1d23b06c2233a287db
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-08 14:18:33 +0900

    byとbrの根本的な打ち間違え

--- staff_add.php
+++ staff_add.php
@@ -6,16 +6,16 @@
 </head>
 <body>
 
-<by>スタッフ追加<by/>
-<by><by/>
+スタッフ追加<br>
+<br>
 <form method="POST" action="staff_add_check.php">
-<by>スタッフ名を入力してください。<by/>
-<by><input type="text" name="name" style="width:200px;"><by/>
-<by>パスワードを入力してください。<by/>
-<by><input type="password" name="pass" style="width:100px ;"><by/>
-<by>パスワードをもう一度入力してください。<by/>
-<by><input type="password" name="pass2" style="width:100px ;"><by/>
-<by><by/>
+スタッフ名を入力してください。<br>
+<input type="text" name="name" style="width:200px;"><br>
+パスワードを入力してください。<br>
+<input type="password" name="pass" style="width:100px ;"><br>
+パスワードをもう一度入力してください。<br>
+<input type="password" name="pass2" style="width:100px ;"><br>
+<br>
 <input type="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
 </form>
