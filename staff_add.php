88752f36ada8092b86447200ffa8c558b1f4df60
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-08 14:08:52 +0900

    改行タグ打ち間違え

--- staff_add.php
+++ staff_add.php
@@ -6,16 +6,16 @@
 </head>
 <body>
 
-スタッフ追加<by />
-<by />
+スタッフ追加<by>
+<by>
 <form method="POST" action="staff_add_check.php">
-スタッフ名を入力してください。
-<input type="text" name="name" style="width:200px;"><by />
-パスワードを入力してください。<by />
-<input type="password" name="pass" style="width:100px ;"><by />
-パスワードをもう一度入力してください。
-<input type="password" name="pass2" style="width:100px ;"><by />
-<by />
+スタッフ名を入力してください。<by>
+<input type="text" name="name" style="width:200px;"><by>
+パスワードを入力してください。<by>
+<input type="password" name="pass" style="width:100px ;"><by>
+パスワードをもう一度入力してください。<by>
+<input type="password" name="pass2" style="width:100px ;"><by>
+<by>
 <input type="button" onclick="history.back()" value="戻る">
 <input type="submit" value="OK">
 </form>
