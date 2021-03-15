f0421f9d7494c5eb272774536823978a3fb88e9c
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-15 11:43:57 +0900

    飛び先と修正、削除ボタン追加

--- staff/staff_list.php
+++ staff/staff_list.php
@@ -11,38 +11,38 @@
 try
 {
 
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
-$dbh = new PDO($dsn, $user, $password);
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+	$user='root';
+	$password='';
+	$dbh = new PDO($dsn, $user, $password);
+	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-$sql='SELECT code, name FROM mst_staff WHERE 1';
-$stmt=$dbh->prepare($sql);
-$stmt->execute();
+	$sql = 'SELECT code, name FROM mst_staff WHERE 1';
+	$stmt = $dbh->prepare($sql);
+	$stmt->execute();
 
-$dbh=null;
+	$dbh=null;
 
-print 'スタッフ一覧<br><br>';
+		print 'スタッフ一覧<br><br>';
 
-print '<form method="post" action="staff_edit.php">';
-while(true)
-{
-$rec=$stmt->fetch(PDO::FETCH_ASSOC);
-if($rec==false)
-  {
-    break;
-  }
-print '<input type="radio" name="staffcode" value="'.$rec['code'].'">';
-print $rec['name'];
-print '<br>';
+		print '<form method="post" action="staff_branch.php">';
+	while(true)
+	{
+		$rec=$stmt->fetch(PDO::FETCH_ASSOC);
+		if($rec==false)
+			{
+				break;
+			}
+	print '<input type="radio" name="staffcode" value="'.$rec['code'].'">';
+	print $rec['name'];
+	print '<br>';
 }
-print '<input type="submit" value="修正">';
+	print '<input type = "submit" name = "edit" value = "修正">';
+	print '<input type = "submit" name = "delete" value = "削除">';
 }
 catch(Exception $e)
 {
-  print 'ただいま障害により大変ご迷惑をお掛けしております。';
-  var_dump($e);
+	print 'ただいま障害により大変ご迷惑をお掛けしております。';
 	exit();
 }
 
