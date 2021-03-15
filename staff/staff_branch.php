8e45a5a25c218c7555914d7e61d03dca1d61f356
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-15 12:54:22 +0900

    URLパラメータでスタッフコードを渡す

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -2,13 +2,15 @@
 
 	if (isset($_POST['edit'])==true)
 	{
-		header('Location:staff_edit.php');
+		$staff_code = $_POST['staffcode'];
+		header('Location:staff_edit.php?staffcode =' .$staff_code);
 		exit();
 	}
 
 	if (isset($_POST['delete'])==true)
 	{
-		header('Location:staff_delete.php');
+		$staff_code = $_POST['staffcode'];
+		header('Location:staff_delete.php?staffcode =' .$staff_code);
 	}
 
 ?>
