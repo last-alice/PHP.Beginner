4baf230399a57a9d3c2a8086ac808cadefec4bcb
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-15 13:06:09 +0900

    遷移ページ記述

--- staff/staff_branch.php
+++ staff/staff_branch.php
@@ -2,6 +2,11 @@
 
 	if (isset($_POST['edit'])==true)
 	{
+		if(isset($_POST['staffcode'])==false)
+		{
+			header('Location:staff_ng.php');
+			exit();
+		}
 		$staff_code = $_POST['staffcode'];
 		header('Location:staff_edit.php?staffcode =' .$staff_code);
 		exit();
@@ -9,8 +14,14 @@
 
 	if (isset($_POST['delete'])==true)
 	{
+		if(isset($_POST['staffcode'])==false)
+		{
+			header('Location:staff_ng.php');
+			exit();
+		}
 		$staff_code = $_POST['staffcode'];
 		header('Location:staff_delete.php?staffcode =' .$staff_code);
+		exit();
 	}
 
 ?>
