31ce4e01347a6e63140b58ee9bb5f5225ae01022
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-18 10:30:31 +0900

    4-2-7の各ページでline1〜17までコード追記

--- product/pro_add_done.php
+++ product/pro_add_done.php
@@ -1,3 +1,20 @@
+<?php
+    session_start();
+    session_regenerate_id(true);
+    if(isset($_SESSION['login'])==false)
+    {
+        print 'ログインされていません。<br>';
+        print '<a href = "../staff_login/staff_login.html">ログイン画面へ</a>';
+        exit();
+    }
+    else
+    {
+        print $_SESSION['staff_name'];
+        print 'さんログイン中<br>';
+        print '<br>';
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -37,7 +54,7 @@ try
 	print 'を追加しました。<br>';
 
 }
-catch(Exception$e)
+catch(Exception $e)
 {
 	print'ただいま障害により大変ご迷惑をお掛けしております。';
 	exit();
@@ -45,7 +62,7 @@ catch(Exception$e)
 
 ?>
 
-	<a href="pro_list.php">戻る</a>
+	<a href = "pro_list.php">戻る</a>
 
 </body>
 </html>
