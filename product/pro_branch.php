16a4293437b342b24a742b92cf6cf73a27cf0a6b
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-18 10:18:57 +0900

    4-2-8のbranchでline3〜11までコード追記

--- product/pro_branch.php
+++ product/pro_branch.php
@@ -1,45 +1,54 @@
 <?php
 
-if(isset($_POST['disp'])==true)
-{
-	if(isset($_POST['procode'])==false)
+	session_start();
+	session_regenerate_id(true);
+	if(isset($_SESSION['login'])==false)
 	{
-		header('Location:pro_ng.php');
+		print 'ログインされていません。<br>';
+		print '<a href="../staff_login/staff_login.html">ログイン画面へ</a>';
 		exit();
 	}
-	$pro_code = $_POST['procode'];
-	header('Location:pro_disp.php?procode='.$pro_code);
-	exit();
-}
 
-if(isset($_POST['add'])==true)
-{
-	header('Location:pro_add.php');
-	exit();
-}
-
-if(isset($_POST['edit'])==true)
-{
-	if(isset($_POST['procode'])==false)
+	if(isset($_POST['disp'])==true)
 	{
-		header('Location:pro_ng.php');
+		if(isset($_POST['procode'])==false)
+		{
+			header('Location:pro_ng.php');
+			exit();
+		}
+		$pro_code = $_POST['procode'];
+		header('Location:pro_disp.php?procode='.$pro_code);
 		exit();
 	}
-	$pro_code = $_POST['procode'];
-	header('Location:pro_edit.php?procode='.$pro_code);
-	exit();
-}
 
-if(isset($_POST['delete'])==true)
-{
-	if(isset($_POST['procode'])==false)
+	if(isset($_POST['add'])==true)
 	{
-		header('Location:pro_ng.php');
+		header('Location:pro_add.php');
 		exit();
 	}
-	$pro_code = $_POST['procode'];
-	header('Location:pro_delete.php?procode='.$pro_code);
-	exit();
-}
+
+	if(isset($_POST['edit'])==true)
+	{
+		if(isset($_POST['procode'])==false)
+		{
+			header('Location:pro_ng.php');
+			exit();
+		}
+		$pro_code = $_POST['procode'];
+		header('Location:pro_edit.php?procode='.$pro_code);
+		exit();
+	}
+
+	if(isset($_POST['delete'])==true)
+	{
+		if(isset($_POST['procode'])==false)
+		{
+			header('Location:pro_ng.php');
+			exit();
+		}
+		$pro_code = $_POST['procode'];
+		header('Location:pro_delete.php?procode='.$pro_code);
+		exit();
+	}
 
 ?>
