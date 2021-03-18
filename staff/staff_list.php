f0d7df61b7a74aa649d8269d818bcd88694e549e
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-18 10:26:48 +0900

    4-2-7の各ページでline1〜17までコード追記

--- staff/staff_list.php
+++ staff/staff_list.php
@@ -1,3 +1,20 @@
+<?php
+    session_start();
+    session_regenerate_id(true);
+    if(isset($_SESSION['login'])==false)
+    {
+        print 'ログインされていません。<br>';
+        print '<a href = "../staff_login/staff_login.html">ログイン画面へ</a>';
+        exit();
+    }
+    else
+    {
+        print $_SESSION['staff_name'];
+        print 'さんログイン中<br>';
+        print '<br>';
+    }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,29 +44,29 @@ try
 	print 'スタッフ一覧<br><br>';
 
 	print '<form method = "post" action = "staff_branch.php">';
-while(true)
-{
-	$rec = $stmt->fetch(PDO::FETCH_ASSOC);
-	if($rec==false)
+	while(true)
 	{
-		break;
+		$rec = $stmt->fetch(PDO::FETCH_ASSOC);
+		if($rec==false)
+		{
+			break;
+		}
+		print '<input type = "radio" name = "staffcode" value = "'.$rec['code'].'">';
+		print $rec['name'];
+		print '<br>';
 	}
-	print '<input type = "radio" name = "staffcode" value = "'.$rec['code'].'">';
-	print $rec['name'];
-	print '<br>';
-}
-	print '<input type = "submit" name = "disp" value = "参照">';
-	print '<input type = "submit" name = "add" value = "追加">';
-	print '<input type = "submit" name = "edit" value = "修正">';
-	print '<input type=  "submit" name = "delete" value = "削除">';
-	print '</form>';
+		print '<input type = "submit" name = "disp" value = "参照">';
+		print '<input type = "submit" name = "add" value = "追加">';
+		print '<input type = "submit" name = "edit" value = "修正">';
+		print '<input type=  "submit" name = "delete" value = "削除">';
+		print '</form>';
 
-}
-catch (Exception $e)
-{
-	print 'ただいま障害により大変ご迷惑をお掛けしております。';
-	exit();
-}
+	}
+	catch (Exception $e)
+	{
+		print 'ただいま障害により大変ご迷惑をお掛けしております。';
+		exit();
+	}
 
 ?>
 
