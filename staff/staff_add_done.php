4d98b4508ea041fe91e55e11669fb5527ae398ff
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-22 11:51:07 +0900

    5-5-2 関数貼り付け

--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -27,31 +27,31 @@
 
 try
 {
+    require_once('../common/common.php');
 
-$staff_name=$_POST['name'];
-$staff_pass=$_POST['pass'];
+	$post=sanitize($_POST);
+    $staff_name=$post['name'];
+    $staff_pass=$post['pass'];
 
-$staff_name=htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
-$staff_pass=htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
-//var_dump("1");
-$dsn='mysql:dbname=shop;host=localhost;charset=utf8';
-$user='root';
-$password='';
+    //var_dump("1");
+    $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+    $user='root';
+    $password='';
 //var_dump("why");
-$dbh = new PDO($dsn, $user, $password);
+    $dbh = new PDO($dsn, $user, $password);
 //var_dump("koko");
-$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 //var_dump("3");
-$sql='INSERT INTO mst_staff (name,password) VALUES (?,?)';
-$stmt=$dbh->prepare($sql);
-$data[]=$staff_name;
-$data[]=$staff_pass;
-$stmt->execute($data);
+    $sql='INSERT INTO mst_staff (name,password) VALUES (?,?)';
+    $stmt=$dbh->prepare($sql);
+    $data[]=$staff_name;
+    $data[]=$staff_pass;
+    $stmt->execute($data);
 //var_dump("4");
-$dbh=null;
+    $dbh=null;
 //var_dump("5");
-print $staff_name;
-print 'さんを追加しました。<br>';
+    print $staff_name;
+    print 'さんを追加しました。<br>';
 
 }
 catch (Exception $e)
