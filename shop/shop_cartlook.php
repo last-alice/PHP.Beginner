696d305c2065cd5ddfa9315516c3af100d3500b0
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-22 14:16:19 +0900

    6-3-5 コードの整理

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -38,28 +38,6 @@ try
     $dbh = new PDO($dsn,$user,$password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-    $sql = 'SELECT name,price,gazou FROM mst_product WHERE code=?';
-    $stmt = $dbh->prepare($sql);
-    $data[] = $pro_code;
-    $stmt->execute($data);
-
-    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-    $pro_name = $rec['name'];
-    $pro_price = $rec['price'];
-    $pro_gazou_name = $rec['gazou'];
-
-    $dbh = null;
-
-    if($pro_gazou_name=='')
-    {
-        $disp_gazou='';
-    }
-    else
-    {
-        $disp_gazou='<img src="../product/gazou/'.$pro_gazou_name.'">';
-    }
-    print '<a href="shop_cartin.php?procode='.$pro_code.'">カートに入れる</a><br><br>';
-
 }
 catch(Exception $e)
 {
@@ -69,20 +47,6 @@ catch(Exception $e)
 
 ?>
 
-    商品情報参照<br>
-        <br>
-    商品コード<br>
-    <?php print $pro_code; ?>
-        <br>
-    商品名<br>
-    <?php print $pro_name; ?>
-        <br>
-    価格<br>
-    <?php print $pro_price; ?>円
-    <br>
-    <?php print $disp_gazou; ?>
-    <br>
-    <br>
     <form>
         <input type = "button" onclick = "history.back()" value = "戻る">
     </form>
