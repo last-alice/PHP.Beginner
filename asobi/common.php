d8599c79e93c30ea7de97afe5faa4f113347be27
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-22 11:37:47 +0900

    5-5-6 新しい関数作成

--- asobi/common.php
+++ asobi/common.php
@@ -24,4 +24,14 @@
 
     return($gengo);
     }
+
+    function sanitize($before)
+    {
+        foreach($before as $key=>$value)
+        {
+            $after[$key]=htmlspecialchars($value,ENT_QUOTES,'UTF-8');
+        }
+    return $after;
+    }
+
 ?>
