1f4af7102b96979212c05436b6a59ae6f87e395d
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-23 13:13:23 +0900

    6-5-2 カートを見るで数量の表記

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -31,6 +31,7 @@ try
 {
 
 	$cart=$_SESSION['cart'];
+	$kazu=$_SESSION['kazu'];
 	$max=count($cart);
 
     $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
@@ -71,12 +72,14 @@ catch(Exception $e)
 
 カートの中身<br>
 <br>
+
 <?php for($i=0;$i<$max;$i++)
 	{
 ?>
 	<?php print $pro_name[$i]; ?>
 	<?php print $pro_gazou[$i]; ?>
 	<?php print $pro_price[$i]; ?>円
+	<?php print $kazu[$i]; ?>
 	<br>
 <?php
 	}
