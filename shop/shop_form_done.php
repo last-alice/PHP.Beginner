629d879b4571785426955d6029414ca8c7c5cc24
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-24 15:20:28 +0900

    7-3-11 メールを送信プログラム

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -34,6 +34,68 @@ try
     print $address.'<br>';
     print $tel.'<br>';
 
+    $honbun='';
+    $honbun.=$onamae."様\n\nこのたびはご注文ありがとうございました。\n";
+    $honbun.="\n";
+    $honbun.="ご注文商品\n";
+    $honbun.="--------------------\n";
+
+    $cart=$_SESSION['cart'];
+    $kazu=$_SESSION['kazu'];
+    $max=count($cart);
+
+    $dsn='mysql:dbname=shop;host=localhost;charset=utf8';
+    $user='root';
+    $password='';
+    $dbh = new PDO($dsn,$user,$password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    for($i=0;$i<$max;$i++)
+    {
+        $sql='SELECT name,price FROM mst_product WHERE code=?';
+        $stmt=$dbh->prepare($sql);
+        $data[0]=$cart[$i];
+        $stmt->execute($data);
+
+        $rec=$stmt->fetch(PDO::FETCH_ASSOC);
+
+        $name=$rec['name'];
+        $price=$rec['price'];
+        $suryo=$kazu[$i];
+        $shokei=$price*$suryo;
+
+        $honbun.=$name.' ';
+        $honbun.=$price.'円 x ';
+        $honbun.=$suryo.'個 = ';
+        $honbun.=$shokei."円\n";
+    }
+
+    $dbh=null;
+
+    $honbun.="送料は無料です。\n";
+    $honbun.="--------------------\n";
+    $honbun.="\n";
+    $honbun.="代金は以下の口座にお振込みください。\n";
+    $honbun.="ろくまる銀行 やさい支店 普通口座 1234567\n";
+    $honbun.="入金確認が取れ次第、梱包、発送させていただきます。\n";
+    $honbun.="\n";
+    $honbun.="□□□□□□□□□□□□□□\n";
+    $honbun.="〜安心野菜のろくまる農園〜\n";
+    $honbun.="\n";
+    $honbun.="○○県六丸郡六丸村123-4\n";
+    $honbun.="電話 090-6060-xxxx\n";
+    $honbun.="メール info@rokumarunouen.co.jp\n";
+    $honbun.="□□□□□□□□□□□□□□\n";
+    //print '<br>';
+    //print nl2br($honbun);
+
+    $title='ご注文ありがとうございます。';
+    $header='From:info@rokumarunouen.co.jp';
+    $honbun=html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
+    mb_language('Japanese');
+    mb_internal_encoding('UTF-8');
+    mb_send_mail($email, $title, $honbun, $header);
+
 }
 catch(Exception $e)
 {
