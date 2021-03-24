f2d3d7c8c2395cf7da74c2f2a88f0b149278d431
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-24 12:11:13 +0900

    7-2-5 フラグ制御

--- shop/shop_form_check.php
+++ shop/shop_form_check.php
@@ -19,9 +19,12 @@
     $address=$post['address'];
     $tel=$post['tel'];
 
+    $okflg=true;
+
     if($onamae=='')
     {
         print 'お名前が入力されていません。<br><br>';
+        $okflg=false;
     }
     else
     {
@@ -33,6 +36,7 @@
     if(preg_match('/\A[\w\-\.]+\@[\w\-\.]+\.([a-z]+)\z/',$email)==0)
     {
         print 'メールアドレスを正確に入力してください。<br><br>';
+        $okflg=false;
     }
     else
     {
@@ -44,6 +48,7 @@
     if(preg_match('/\A[0-9]+\z/',$postal1)==0)
     {
         print '郵便番号は半角数字で入力してください。<br><br>';
+        $okflg=false;
     }
     else
     {
@@ -57,11 +62,13 @@
     if(preg_match('/\A[0-9]+\z/',$postal2)==0)
     {
         print '郵便番号は半角数字で入力してください。<br><br>';
+        $okflg=false;
     }
 
     if($address=='')
     {
         print '住所が入力されていません。<br><br>';
+        $okflg=false;
     }
     else
     {
@@ -73,6 +80,7 @@
     if(preg_match('/\A\d{2,5}-?\d{2,5}-?\d{4,5}\z/',$tel)==0)
     {
         print '電話番号を正確に入力してください。<br><br>';
+        $okflg=false;
     }
     else
     {
@@ -81,16 +89,25 @@
         print'<br><br>';
     }
 
-    print '<form method="post" action="shop_form_done.php">';
-    print '<input type="hidden" name="onamae" value="'.$onamae.'">';
-    print '<input type="hidden" name="email" value="'.$email.'">';
-    print '<input type="hidden" name="postal1" value="'.$postal1.'">';
-    print '<input type="hidden" name="postal2" value="'.$postal2.'">';
-    print '<input type="hidden" name="address" value="'.$address.'">';
-    print '<input type="hidden" name="tel" value="'.$tel.'">';
-    print '<input type="button" onclick="history.back()" value="戻る">';
-    print '<input type="submit" value="OK"><br>';
-    print '</form>';
+    if($okflg==true)
+    {
+        print '<form method="post" action="shop_form_done.php">';
+        print '<input type="hidden" name="onamae" value="'.$onamae.'">';
+        print '<input type="hidden" name="email" value="'.$email.'">';
+        print '<input type="hidden" name="postal1" value="'.$postal1.'">';
+        print '<input type="hidden" name="postal2" value="'.$postal2.'">';
+        print '<input type="hidden" name="address" value="'.$address.'">';
+        print '<input type="hidden" name="tel" value="'.$tel.'">';
+        print '<input type="button" onclick="history.back()" value="戻る">';
+        print '<input type="submit" value="OK"><br>';
+        print '</form>';
+    }
+    else
+    {
+        print'<form>';
+        print'<input type="button" onclick="history.back()" value="戻る">';
+        print'</form>';
+    }
 
 ?>
 
