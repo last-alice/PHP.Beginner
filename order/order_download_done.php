66e7abaa2a5194eebc9981cb0b494101fa52bf51
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-25 17:46:07 +0900

    8-2-8 csvタイポ

--- order/order_download_done.php
+++ order/order_download_done.php
@@ -39,28 +39,28 @@ try
 	$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 	$sql='
-		SELECT
-			dat_sales.code,
-			dat_sales.date,
-			dat_sales.code_member,
-			dat_sales.name AS dat_sales_name,
-			dat_sales.email,
-			dat_sales.postal1,
-			dat_sales.postal2,
-			dat_sales.address,
-			dat_sales.tel,
-			dat_sales_product.code_product,
-			mst_product.name AS mst_product_name,
-			dat_sales_product.prise,
-			dat_sales_product.quantity,
-		FROM
-			dat_sales,dat_sales_product,mst_product
-		WHERE
-			dat_sales.code=dat_sales_product.code_sales
-			AND dat_sales_product.code_product=mst_product.code
-			AND substr(dat_sales.date,1,4)=?
-			AND substr(dat_sales.date,6,2)=?
-			AND substr(dat_sales.date,9,2)=?
+	SELECT
+		dat_sales.code,
+		dat_sales.date,
+		dat_sales.code_member,
+		dat_sales.name AS dat_sales_name,
+		dat_sales.email,
+		dat_sales.postal1,
+		dat_sales.postal2,
+		dat_sales.address,
+		dat_sales.tel,
+		dat_sales_product.code_product,
+		mst_product.name AS mst_product_name,
+		dat_sales_product.price,
+		dat_sales_product.quantity
+	FROM
+	dat_sales,dat_sales_product,mst_product
+	WHERE
+		dat_sales.code=dat_sales_product.code_sales
+		AND dat_sales_product.code_product=mst_product.code
+		AND substr(dat_sales.date,1,4)=?
+		AND substr(dat_sales.date,6,2)=?
+		AND substr(dat_sales.date,9,2)=?
 	';
 	$stmt=$dbh->prepare($sql);
 	$data[]=$year;
