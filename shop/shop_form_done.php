e030bdaeef5f1593fbc5da206393aaf15b753802
last_alice <iolite.kinseiseki18@gmail.com>
2021-03-25 11:19:26 +0900

    7-5-4 ロック、アンロックとトップページ遷移

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -71,6 +71,10 @@ try
         $honbun.=$shokei."円\n";
     }
 
+    $sql='LOCK TABLES dat_sales WRITE,dat_sales_product WRITE';
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute();
+
     $sql='INSERT INTO dat_sales(code_member,name,email,postal1,postal2,address,tel) VALUES(?,?,?,?,?,?,?)';
     $stmt=$dbh->prepare($sql);
     $data=array();
@@ -100,6 +104,10 @@ try
         $data[]=$kazu[$i];
         $stmt->execute($data);
     }
+
+    $sql='UNLOCK TABLES';
+    $stmt=$dbh->prepare($sql);
+    $stmt->execute();
 
     $dbh=null;
 
@@ -142,6 +150,9 @@ catch(Exception $e)
 }
 
 ?>
+
+<br>
+<a href="shop_list.php">商品画面へ</a>
 
 </body>
 </html>
